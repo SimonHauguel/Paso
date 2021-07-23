e17d204e393ef41217e1d21ff0daa21896d5246c
SimonHauguel <simon.hauguel@student.junia.com>
2021-07-23 22:15:34 +0200

    Review pure if parser

--- src/Paso/Parser/AST/Expr.hs
+++ src/Paso/Parser/AST/Expr.hs
@@ -3,11 +3,12 @@ module Paso.Parser.AST.Expr where
 import Paso.Parser.AST.Match
 import Data.List.NonEmpty
 
-type TupleIf = (Expr, NonEmpty (MatchTo Expr))
+type TupleIf = (Expr, NonEmpty ToExpr)
+type ToExpr = MatchTo Expr
 
 data Expr
-  = If Expr (NonEmpty (MatchTo Expr))
-  | Let (NonEmpty (MatchTo Expr))
+  = If Expr (NonEmpty ToExpr)
+  | Let (NonEmpty ToExpr)
   | TestExpr -- Just to test some parser without be annoying with parse complex expression
              -- Must be deleted in the future
   deriving Show
--- src/Paso/Parser/IfThenElse.hs
+++ src/Paso/Parser/IfThenElse.hs
@@ -1,29 +1,34 @@
 module Paso.Parser.IfThenElse where
 
-import qualified Text.Megaparsec as MG
-import qualified Paso.Language.Tokens as TK
-import Paso.Program.Types
-import Paso.Parser.ParserData
-import Paso.Parser.Utils
-import Paso.Parser.AST.Match
-import Paso.Parser.AST.Expr
-import Data.Functor (($>))
-import Data.List.NonEmpty (NonEmpty(..))
-import Paso.Parser.Expr
+import qualified Text.Megaparsec               as MG
+import qualified Paso.Language.Tokens          as TK
+import           Paso.Program.Types
+import           Paso.Parser.ParserData
+import           Paso.Parser.Utils
+import           Paso.Parser.AST.Match
+import           Paso.Parser.AST.Expr
+import           Data.Functor                   ( ($>) )
+import           Data.List.NonEmpty             ( NonEmpty(..) )
+import           Paso.Parser.Expr
+
 
 ifParse :: Parser Expr
 ifParse = (tok TK.If $> uncurry If) <*> ifPur
 
 ifPur :: Parser TupleIf -- if a | b | c
 ifPur = do
-  cond <- expr
-  first <- tok TK.Pipe *> expr
+  cond   <- expr
+  first  <- tok TK.Pipe *> expr
   second <- tok TK.Pipe *> expr
-  undefined
-  -- pure (cond, mkRes "True" first :| [mkRes "False" second])
+  pure
+    ( cond
+    , NonIrrefutable (Left $ Value "True")
+    :~~>: first
+    :|    [Irrefutable Ignore :~~>: second]
+    )
 
 ifMulti :: Parser TupleIf -- if | cond => a | cond => b
 ifMulti = do
-  _ <- tok TK.Pipe
+  _        <- tok TK.Pipe
   listeRes <- MG.sepBy1 expr (tok TK.Pipe)
   undefined
