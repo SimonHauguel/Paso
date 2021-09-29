c7fb6edf0d68b2773f256b1d26c20521db622d9e
SimonHauguel <simon.hauguel@student.junia.com>
2021-09-29 13:38:19 +0200

    fully functionnal type-annotation and constraint parser

--- src/Paso/Parser/TypesAnnotations.hs
+++ src/Paso/Parser/TypesAnnotations.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE LambdaCase #-}
-
 module Paso.Parser.TypesAnnotations where
 
 import qualified Text.Megaparsec               as MG
@@ -7,13 +5,11 @@ import           Text.Megaparsec                ( (<|>)
                                                 , try
                                                 )
 import           Paso.Parser.ParserData
-import           Paso.Program.Types
 import           Paso.Program.Context
 import qualified Paso.Parser.AST.Constraint    as CN
 import           Paso.Parser.AST.TypesRepresentation
 import           Paso.Parser.Utils
 import           Paso.Language.Tokens
-import           Data.Bifunctor                 ( bimap )
 import           Data.Function                  ( on )
 import           Data.Set                       ( fromList
                                                 , singleton
@@ -21,26 +17,24 @@ import           Data.Set                       ( fromList
                                                 )
 
 typeAnnotation :: Parser PasoType -- TODO : Add recursive Arrow type (HOF syntax)
-typeAnnotation = (,) <$> constraint <*> (foldr1 Fun <$> typeParser)
+typeAnnotation = (,) <$> constraint <*> subTypeArrow
  where
   typeParser = MG.sepBy1
     (   (Unique . getName <$> tok typeNameTok)
     <|> (SucredIso . getName <$> (tok Iso *> tok typeNameTok))
+    <|> parens subTypeArrow
     )
     (tok ArrowRight)
 
-  constraint = MG.option
-    empty
-    (  (multiConstraint <|> (singleton <$> isoSingleConstraint))
-    <* tok BigArrowRight
-    )
+  constraint   = MG.option empty (try $ multiConstraint <* tok BigArrowRight)
 
+  subTypeArrow = foldr1 Fun <$> typeParser
 
 
 multiConstraint :: Parser (Context CN.Constraint)
 multiConstraint = multi <|> single
  where
-  subParseMulti = fromList <$> MG.sepBy1 isoSingleConstraint (tok SemiColon)
+  subParseMulti = fromList <$> MG.sepBy isoSingleConstraint (tok SemiColon)
   multi         = MG.between (tok OpenBracket) (tok CloseBracket) subParseMulti
   single        = singleton <$> isoSingleConstraint
 
